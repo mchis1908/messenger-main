58869346a89afdc423487a5c76cb0485883c5827
mchis1908 <mchis19.08@gmail.com>
2023-11-04 19:40:31 +0700

    updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,42 +4,36 @@ import User from "../components/User";
 import Navigation from "../components/Navigation";
 import { UserType } from "../UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 const HomeScreen = () => {
   const [users, setUsers] = useState([]);
-  const { userId, setUserId } = useState('1');
+  const [userId, setUserId] = useState(null); // Use useState directly
 
   useEffect(() => {
-      const fetchUsers = async () => {
-      await AsyncStorage.getItem("userId")
-      .then((userId) => {
-          console.log(userId);
-      })
-      .catch((error) => {
-        console.log("Error retrieving userId:", error);
-      });
-
-      await axios
-        .get(`http://192.168.1.12:8383/user/all/${userId}`)
-        .then((response) => {
-          setUsers(response.data);
-          console.log('response', response)
-        })
-        .catch((error) => {
-          console.log("error retrieving users", error);
-        });
+    const fetchUsers = async () => {
+      try {
+        const storedUserId = await AsyncStorage.getItem("userId");
+        setUserId(storedUserId); // No need to use await here
+  
+        // Fetch and set users based on userId
+        const response = await axios.get(`http://192.168.1.12:8383/user/all/${storedUserId}`);
+        setUsers(response.data);
+      } catch (error) {
+        console.log("Error:", error);
+      }
     };
-
     fetchUsers();
   }, []);
-  console.log("users", users);
+
+  // This will be logged after users state has been set
   return (
     <View style={{flex:1}}>
-      <View style={{ padding: 25 }}>
+      {/* <View style={{ padding: 25 }}>
         {users.map((item, index) => (
           <User key={index} item={item} />
         ))}
-      </View>
+      </View> */}
       <View style={{position:'absolute', bottom:0, width:'100%'}}>
         <Navigation/>
       </View>
