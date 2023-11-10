1d9dd9738f30f5b8ee5a0ff2256d9a333725b617
TKhai55 <thienkhaiak5555@gmail.com>
2023-11-10 08:49:51 +0700

    aa

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { View, Text, Pressable, Alert, Image } from 'react-native'; // Import thêm Image
+import { View, Text, Pressable, Image, Modal, FlatList, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { UserType } from "../UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -7,9 +7,8 @@ import { EXPO_PUBLIC_URL } from '@env';
 import axios from "axios";
 import { Avatar } from 'react-native-elements';
 import * as ImagePicker from 'expo-image-picker';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { getStorage, ref, uploadBytes, getDownloadURL, list } from 'firebase/storage';
 import { FIREBASE_APP } from '../FirebaseConfig';
-import * as FileSystem from 'expo-file-system';
 import { doc, updateDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 
@@ -18,6 +17,20 @@ const Profile = () => {
   const [users, setUsers] = useState({});
   const { userId, setUserId } = useContext(UserType);
   const [image, setImage] = useState(null);
+  const [avatarList, setAvatarList] = useState([]);
+  const [isAvatarModalVisible, setIsAvatarModalVisible] = useState(false);
+
+  const toggleAvatarModal = () => {
+    setIsAvatarModalVisible(!isAvatarModalVisible);
+  };
+
+  const updateAvatarList = async () => {
+    const storage = getStorage(FIREBASE_APP);
+    const storageRef = ref(storage, `profile_images/${userId}`);
+    const listResult = await list(storageRef);
+    const avatarUrls = await Promise.all(listResult.items.map((item) => getDownloadURL(item)))
+    setAvatarList(avatarUrls);
+  }
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -27,6 +40,9 @@ const Profile = () => {
 
         const response = await axios.get(`${EXPO_PUBLIC_URL}/user/${storedUserId}`);
         setUsers(response.data);
+
+        // Lấy danh sách avatar từ Firebase Storage
+        updateAvatarList();
       } catch (error) {
         console.log("Error:", error);
       }
@@ -34,8 +50,8 @@ const Profile = () => {
     fetchUsers();
   }, []);
 
-
-  const handleImageUpload = async () => {
+  const uploadImageAndRefreshList = async () => {
+    const timestamp = new Date()
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -47,39 +63,47 @@ const Profile = () => {
       const storage = getStorage(FIREBASE_APP);
       const db = getFirestore(FIREBASE_APP);
 
-      const fileName = `profile_${userId}.jpg`;
-      const storageRef = ref(storage, `profile_images/${fileName}`);
+      const fileName = `profile_${userId}_${timestamp.getTime()}.jpg`;
+      const storageRef = ref(storage, `profile_images/${userId}/${fileName}`);
       try {
         const response = await fetch(result.assets[0].uri);
         const blob = await response.blob();
-
-
-        // Upload the image to Firebase Storage
         const uploadTask = uploadBytes(storageRef, blob);
-
         await uploadTask;
-
-        // Get the download URL for the uploaded image
         const url = await getDownloadURL(storageRef);
 
-        // Update the user's profile image URL in Firestore
         const userDocRef = doc(db, "users", userId);
         const updateData = {
           image: url,
         };
-
         await updateDoc(userDocRef, updateData);
-
-        // Update the state with the new image URL to trigger a re-render
         setImage(url);
+
+        // Cập nhật danh sách avatar sau khi tải lên
+        updateAvatarList();
       } catch (error) {
         console.error("Error during image upload:", error);
       }
     }
+    // Đóng modal sau khi tải hình lên
+    setIsAvatarModalVisible(false);
   };
-  
 
-
+  const handleSelectNewAvatar = async (avatarUrl) => {
+    setImage(avatarUrl);
+    try {
+      const db = getFirestore(FIREBASE_APP);
+      const userDocRef = doc(db, "users", userId);
+      const updateData = {
+        image: avatarUrl,
+      };
+      await updateDoc(userDocRef, updateData);
+    } catch (error) {
+      console.error("Error updating user image:", error);
+    }
+    // Đóng modal sau khi chọn xong
+    setIsAvatarModalVisible(false);
+  };
 
   return (
     <View style={{
@@ -89,16 +113,41 @@ const Profile = () => {
       alignItems: 'center',
     }}>
 
-<Avatar
+      <Avatar
         rounded
-        source={{ uri: image || users.image }}  // Use the 'image' state as the source
-        size="large"
+        source={{ uri: image || users.image }}
+        size="xlarge"
+        onPress={toggleAvatarModal} // Mở modal khi nhấn vào avatar
       />
-      <Pressable onPress={handleImageUpload}>
+
+      <Modal
+        visible={isAvatarModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={toggleAvatarModal}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: 300 }}>
+            <Text>Select a New Avatar</Text>
+            <FlatList
+              data={avatarList}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => handleSelectNewAvatar(item)}>
+                  <Image source={{ uri: item }} style={{ width: 100, height: 100 }} />
+                </TouchableOpacity>
+              )}
+            />
+            <TouchableOpacity onPress={toggleAvatarModal}>
+              <Text>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Pressable onPress={uploadImageAndRefreshList}>
         <Text>Upload Image</Text>
       </Pressable>
       <Text>{users.name}</Text>
-      {/* Các tab chuyển sang các stack khác */}
       <Pressable
         onPress={() => navigation.navigate("Setting")}
         style={buttonStyle}
@@ -124,7 +173,7 @@ const Profile = () => {
         <Text style={buttonTextStyle}>Log Out</Text>
       </Pressable>
     </View>
-  )
+  );
 }
 
 const buttonStyle = {
